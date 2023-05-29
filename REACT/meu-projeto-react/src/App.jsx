6fa78a0615143b89f8a3f7871996802f6693b9d0
JoaoCavalcanteS <joaovitorcavalcante2012@gmail.com>
2023-05-28 22:11:14 -0300

    Estados e Eventos - Renderização Condicional

--- REACT/meu-projeto-react/src/App.jsx
+++ REACT/meu-projeto-react/src/App.jsx
@@ -2,27 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import MeuComponente from './components/MeuComponente'
+import MeuContador from './MeuContador'
 
 function App() {
 
   return (
     <div>
-      <h1>Olá mundo, React!</h1>
-      <MeuComponente/>
-
-      <MeuBotao conteudo='me clique'/>
-      <MeuBotao conteudo='depois aqui'/>
-      <MeuBotao conteudo='e por fim aqui'/>
-
+      <MeuContador/>
     </div>
-    
-  )
-}
-
-function MeuBotao(props){
-  console.log(props);
-  return(
-    <button>{props.conteudo}</button>
   )
 }
 
---
+++ REACT/meu-projeto-react/src/MeuContador.jsx
@@ -0,0 +1,34 @@
+//Usando o ESTADO para atualizar o valor, usando um ruk?
+import { useState } from "react";
+
+export default function MeuContador(){
+
+
+    const [contador, setContador] = useState(0);
+
+    function aumentar(){
+        setContador(contador + 1);
+    }
+    function diminuir(){
+        setContador(contador - 1);
+    }
+    if(contador>5){
+        return(
+            <div>
+                <h1>Contador maior que 5</h1>
+                <button onClick={aumentar}>aumentar</button>
+                <button onClick={diminuir}>diminuir</button>
+            </div>
+        )
+    }
+
+    return(
+        <div>
+            <h1>Meu contador:{contador}</h1>
+                {contador>9? <h3>Valor muito grande</h3> : null }
+                <button onClick={aumentar}>aumentar</button>
+                <button onClick={diminuir}>diminuir</button>
+        </div>
+    )
+    //renderização condicional
+}
