f38c6f10f49fbc321bd13483cc598022b975f06c
JoaoCavalcanteS <joaovitorcavalcante2012@gmail.com>
2023-05-28 23:54:42 -0300

    Update App.jsx

--- REACT/meu-projeto-react/src/App.jsx
+++ REACT/meu-projeto-react/src/App.jsx
@@ -1,19 +1,55 @@
-import { useState } from 'react'
+import {useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import MeuComponente from './components/MeuComponente'
 import MeuContador from './MeuContador'
 import MinhaLista from './components/MinhaLista'
 
-function App() {
+
+const minhaLista = [
+  {id:'1', value:'Fruta'},
+  {id:'2', value:'Legume'},
+  {id:'3', value:'Carne'},
+  {id:'4', value:'Doce'}
+]
+
+
+export default function App() {
+
+  const [produtos, setProdutos] = useState(minhaLista);
+  const [pesquisa, setPesquisa] = useState('');
+
+  useEffect(
+    ()=> {
+        if(pesquisa){
+        const novaLista = minhaLista.filter((item)=>{
+          return item.value.toLowerCase().includes(pesquisa.toLowerCase())
+        })
+        setProdutos(novaLista)
+      }else{
+        setProdutos(minhaLista)
+      }
+    }
+    //toda vez que ocorrer a arrow function, atualize isso
+  , [pesquisa])
 
   return (
     <div>
-      <h1>Listas no React</h1>
-      <MinhaLista/>
+      <h1>Efeitos colaterais</h1>
+      <input type="text" 
+      value={pesquisa}
+      onChange={(e) => setPesquisa(e.target.value)}
+      placeholder='pesquise aqui'></input>
+
+      {minhaLista.map((item) => {
+        return(
+            <div key={item.id}>
+                <h4>{item.value}</h4>
+            </div>
+        )
+    })}
     </div>
   )
 }
 
 
-export default App
